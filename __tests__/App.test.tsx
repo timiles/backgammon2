a3bc07316d8cfe2176597487078e85248a3bd6a8
Tim Iles <tim@timiles.com>
2019-10-16 20:20:42 +0100

    Move App.tsx into src folder

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import 'react-native';
 import * as renderer from 'react-test-renderer';
-import App from '../App';
+import App from '../src/App';
 
 it('renders correctly', () => {
   renderer.create(<App />);
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as Expo from 'expo';
+import * as React from 'react';
+import App from './App';
+
+function Root() {
+  return (
+    <App />
+  );
+}
+
+export default Expo.registerRootComponent(Root);
