351623574d46a8c07669f467eb92248b1575ff68
Tim Iles <tim@timiles.com>
2019-12-14 23:41:51 +0000

    Move App config code into Root

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
 import Board from './components/Board';
-import * as StoreModule from './store';
 
 export default function App() {
-  const rootReducer = combineReducers<StoreModule.ApplicationState>(StoreModule.reducers);
-  const store = createStore(rootReducer);
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Board />
-      </View>
-    </Provider>
+    <Board />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    maxHeight: 411,
-    maxWidth: 731,
-  },
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import * as Expo from 'expo';
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
 import App from './App';
+import * as StoreModule from './store';
 
 function Root() {
+  const rootReducer = combineReducers<StoreModule.ApplicationState>(StoreModule.reducers);
+  const store = createStore(rootReducer);
   return (
-    <App />
+    <Provider store={store}>
+      <View style={styles.mobileSize}>
+        <App />
+      </View>
+    </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  mobileSize: {
+    flex: 1,
+    maxHeight: 411,
+    maxWidth: 731,
+  },
+});
 
 export default Expo.registerRootComponent(Root);
