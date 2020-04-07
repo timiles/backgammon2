aa81f6239a4b49fd65736e4b1fb1833d73b19641
Tim Iles <tim@timiles.com>
2020-04-07 22:26:45 +0100

    Refactor Dice as a stateless function

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -13,46 +13,37 @@ interface IOwnProps {
 
 type Props = IOwnProps & StateProps & DispatchProps;
 
-class Dice extends React.Component<Props> {
-  handleRollDie = () => {
-    const { player, currentPlayer, rollInitialDie, rollDice } = this.props;
-    if (currentPlayer == null) {
-      rollInitialDie(player);
-    } else {
-      rollDice(player);
-    }
-  };
+function Dice(props: Props) {
+  const {
+    player, dice: [die1, die2], requiresReroll, currentPlayer,
+  } = props;
 
-  render() {
-    const {
-      player, dice: [die1, die2], requiresReroll, currentPlayer,
-    } = this.props;
-
-    if (currentPlayer == null) {
-      return (
-        <View style={styles.diceContainer}>
-          {die2 != null && <Die player={player} value={die2.value} isSpent />}
-          <Die
-            player={player}
-            value={die1 ? die1.value : '?'}
-            onPress={(die1 == null || requiresReroll) ? this.handleRollDie : null}
-          />
-        </View>
-      );
-    }
-
-    const clickAction = (currentPlayer === player) ? this.handleRollDie : null;
+  if (currentPlayer == null) {
+    const handlePress = (die1 == null || requiresReroll)
+      ? () => props.rollInitialDie(player) : null;
     return (
       <View style={styles.diceContainer}>
-        {die1 == null
-          ? <Die player={player} value="?" onPress={clickAction} />
-          : <Die player={player} value={die1.value} isSpent={die1.isSpent} />}
-        {die2 == null
-          ? <Die player={player} value="?" onPress={clickAction} />
-          : <Die player={player} value={die2.value} isSpent={die2.isSpent} />}
+        {die2 != null && <Die player={player} value={die2.value} isSpent />}
+        <Die
+          player={player}
+          value={die1 ? die1.value : '?'}
+          onPress={handlePress}
+        />
       </View>
     );
   }
+
+  const handlePress = (currentPlayer === player) ? () => props.rollDice(player) : null;
+  return (
+    <View style={styles.diceContainer}>
+      {die1 == null
+        ? <Die player={player} value="?" onPress={handlePress} />
+        : <Die player={player} value={die1.value} isSpent={die1.isSpent} />}
+      {die2 == null
+        ? <Die player={player} value="?" onPress={handlePress} />
+        : <Die player={player} value={die2.value} isSpent={die2.isSpent} />}
+    </View>
+  );
 }
 
 const mapStateToProps = ({ dice, player }: ApplicationState, ownProps: IOwnProps) => (
