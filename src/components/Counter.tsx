0259b4621d29bf73693a46d826c50d395023b3f9
Tim Iles <tim@timiles.com>
2024-02-14 15:12:17 +0000

    Extract utility methods from Counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,20 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
-import {
-  Animated,
-  GestureResponderHandlers,
-  PanResponder,
-  PanResponderGestureState,
-} from 'react-native';
+import { Animated, GestureResponderHandlers } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 import colors from '../colors';
 import { CounterModel } from '../models/CounterModel';
 import Player from '../models/Player';
 import { RootState } from '../store';
-import { BarIndexes } from '../store/Board';
 import { moveCounter } from '../store/actions';
 import styles from '../styles';
-import { getDistance, getOtherPlayer } from '../utils';
+import { canMoveCounter, createGestureResponderHandlers, findDestinationIndex } from '../utils';
 
 interface IProps extends CounterModel {
   pointIndex: number;
@@ -35,86 +29,39 @@ export default function Counter(props: IProps) {
   const [gestureResponderHandlers, setGestureResponderHandlers] =
     useState<GestureResponderHandlers | null>();
 
-  const canMove = (destinationIndex?: number) => {
-    if (player !== currentPlayer) {
-      return false;
-    }
-
-    const barIndex = BarIndexes[player];
-    if (points[barIndex].counters.length > 0 && barIndex !== pointIndex) {
-      // Player has counters on the bar, and this is not their bar
-      return false;
-    }
-
-    if (destinationIndex != null) {
-      if (destinationIndex === pointIndex) {
-        // Can't move to the same point
-        return false;
-      }
-
-      const otherPlayer = getOtherPlayer(player);
-      if (points[destinationIndex].counters.filter((x) => x.player === otherPlayer).length > 1) {
-        // Destination is blocked by other player
-        return false;
-      }
-
-      const distance = getDistance(player, pointIndex, destinationIndex);
-      if (!dice.some((x) => x.value === distance && x.remainingMoves > 0)) {
-        // Don't have the dice roll available
-        return false;
-      }
-    }
-
-    return true;
-  };
-
   useEffect(() => {
     let handlers: GestureResponderHandlers | null = null;
 
-    if (canMove()) {
-      const useNativeDriver = false;
-      handlers = PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onPanResponderStart: () => {
-          onSourceChange(true);
-        },
-        onPanResponderMove: Animated.event(
-          [null, { dx: counterLocation.x, dy: counterLocation.y }],
-          {
-            useNativeDriver,
-          },
-        ),
-        onPanResponderRelease: (_, gestureState: PanResponderGestureState) => {
-          const { moveX, moveY } = gestureState;
-          const destinationIndex = points
-            .map((x) => x.box)
-            .findIndex(
-              (x) => x.left < moveX && moveX < x.right && x.top < moveY && moveY < x.bottom,
-            );
-          if (destinationIndex >= 0 && canMove(destinationIndex)) {
-            dispatch(
-              moveCounter({
-                id,
-                player,
-                sourceIndex: pointIndex,
-                destinationIndex,
-                isLastMove: dice[0].remainingMoves + dice[1].remainingMoves === 1,
-              }),
-            );
+    if (player === currentPlayer && canMoveCounter(player, dice, points, pointIndex)) {
+      const findDestinationId = (x: number, y: number) => findDestinationIndex(points, x, y);
 
-            onSourceChange(false);
-          } else {
-            const config: Animated.SpringAnimationConfig = {
-              toValue: { x: 0, y: 0 },
-              friction: 5,
-              useNativeDriver,
-            };
-            Animated.spring(counterLocation, config).start(() => {
-              onSourceChange(false);
-            });
-          }
-        },
-      }).panHandlers;
+      const canMoveToDestination = (destinationIndex: number) =>
+        destinationIndex >= 0 && canMoveCounter(player, dice, points, pointIndex, destinationIndex);
+
+      const handleMoveStart = () => onSourceChange(true);
+
+      const handleMoveSuccess = (destinationIndex: number) => {
+        dispatch(
+          moveCounter({
+            id,
+            player,
+            sourceIndex: pointIndex,
+            destinationIndex,
+            isLastMove: dice[0].remainingMoves + dice[1].remainingMoves === 1,
+          }),
+        );
+      };
+
+      const handleMoveEnd = () => onSourceChange(false);
+
+      handlers = createGestureResponderHandlers(
+        findDestinationId,
+        canMoveToDestination,
+        counterLocation,
+        handleMoveStart,
+        handleMoveSuccess,
+        handleMoveEnd,
+      );
     }
 
     setGestureResponderHandlers(handlers);
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,102 @@
+import {
+  Animated,
+  GestureResponderHandlers,
+  PanResponder,
+  PanResponderGestureState,
+} from 'react-native';
+
+import { CounterContainerModel } from './models/CounterContainerModel';
+import { DieModel } from './models/DieModel';
 import { DieValue } from './models/DieValue';
 import Player from './models/Player';
+import { BarIndexes } from './store/Board';
+
+export function canMoveCounter(
+  player: Player,
+  dice: DieModel[],
+  points: CounterContainerModel[],
+  sourceIndex: number,
+  destinationIndex?: number,
+): boolean {
+  const barIndex = BarIndexes[player];
+  if (points[barIndex].counters.length > 0 && barIndex !== sourceIndex) {
+    // Player has counters on the bar, and this is not their bar
+    return false;
+  }
+
+  if (destinationIndex != null) {
+    if (destinationIndex === sourceIndex) {
+      // Can't move to the same point
+      return false;
+    }
+
+    const otherPlayer = getOtherPlayer(player);
+    if (points[destinationIndex].counters.filter((x) => x.player === otherPlayer).length > 1) {
+      // Destination is blocked by other player
+      return false;
+    }
+
+    const distance = getDistance(player, sourceIndex, destinationIndex);
+    if (!dice.some((x) => x.value === distance && x.remainingMoves > 0)) {
+      // Don't have the dice roll available
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function createGestureResponderHandlers<T>(
+  getLocationId: (x: number, y: number) => T,
+  canMoveToLocationId: (locationId: T) => boolean,
+  currentLocation: Animated.ValueXY,
+  onMoveStart: () => void,
+  onMoveSuccess: (locationId: T) => void,
+  onMoveEnd: () => void,
+): GestureResponderHandlers {
+  const useNativeDriver = false;
+
+  return PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderStart: () => {
+      onMoveStart();
+    },
+    onPanResponderMove: Animated.event([null, { dx: currentLocation.x, dy: currentLocation.y }], {
+      useNativeDriver,
+    }),
+    onPanResponderRelease: (_, gestureState: PanResponderGestureState) => {
+      const { moveX, moveY } = gestureState;
+      const locationId = getLocationId(moveX, moveY);
+      if (canMoveToLocationId(locationId)) {
+        // If we can move, report success and end
+        onMoveSuccess(locationId);
+        onMoveEnd();
+      } else {
+        // Otherwise animate back to starting position, then end
+        const config: Animated.SpringAnimationConfig = {
+          toValue: { x: 0, y: 0 },
+          friction: 5,
+          useNativeDriver,
+        };
+        Animated.spring(currentLocation, config).start(() => {
+          onMoveEnd();
+        });
+      }
+    },
+  }).panHandlers;
+}
+
+export function findDestinationIndex(
+  points: CounterContainerModel[],
+  locationX: number,
+  locationY: number,
+): number {
+  return points
+    .map((x) => x.box)
+    .findIndex(
+      (x) => x.left < locationX && locationX < x.right && x.top < locationY && locationY < x.bottom,
+    );
+}
 
 export function getOtherPlayer(player: Player): Player {
   return ((player + 1) % 2) as Player;
