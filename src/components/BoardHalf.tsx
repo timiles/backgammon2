278614a0e641d876d65d560b98b6bb43d61ab988
Tim Iles <tim@timiles.com>
2024-02-14 15:13:52 +0000

    Refactor class to functional component

--- src/components/BoardHalf.tsx
+++ src/components/BoardHalf.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 import Home from './Home';
@@ -12,64 +12,54 @@ interface IProps {
   side: Side;
 }
 
-interface IState {
-  sourceCount: number;
-}
+export default function BoardHalf(props: IProps) {
+  const { side } = props;
 
-export default class BoardHalf extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
+  const [sourceCount, setSourceCount] = useState(0);
 
-    this.state = { sourceCount: 0 };
-  }
-
-  handleSourceChange = (isSource: boolean) => {
+  const handleSourceChange = (isSource: boolean) => {
     // Increment or decrement sourceCount, to handle asynchronous UI updates correctly
-    this.setState((prevState) => ({ sourceCount: prevState.sourceCount + (isSource ? 1 : -1) }));
+    setSourceCount((prevSourceCount) => prevSourceCount + (isSource ? 1 : -1));
   };
 
-  render() {
-    const { side } = this.props;
-    // Top side:    12|11|10| 9| 8| 7|bar (0)| 6| 5| 4| 3| 2| 1
-    // Bottom side: 13|14|15|16|17|18|bar(24)|19|20|21|22|23|24
-    const startingPointIndex = side === 'top' ? 12 : 13;
-    const direction = side === 'top' ? -1 : 1;
-    const barIndex = BarIndexes[side === 'top' ? Player.Red : Player.Black];
+  // Top side:    12|11|10| 9| 8| 7|bar (0)| 6| 5| 4| 3| 2| 1
+  // Bottom side: 13|14|15|16|17|18|bar(24)|19|20|21|22|23|24
+  const startingPointIndex = side === 'top' ? 12 : 13;
+  const direction = side === 'top' ? -1 : 1;
+  const barIndex = BarIndexes[side === 'top' ? Player.Red : Player.Black];
 
-    const { sourceCount } = this.state;
-    const sourceStyle = sourceCount > 0 ? styles.draggableSource : null;
+  const sourceStyle = sourceCount > 0 ? styles.draggableSource : null;
 
-    const leftHandPoints: JSX.Element[] = [];
-    const rightHandPoints: JSX.Element[] = [];
-    for (let i = 0; i < 6; i += 1) {
-      const pointIndex = startingPointIndex + i * direction;
-      leftHandPoints.push(
-        <Point
-          key={i}
-          type="Point"
-          index={pointIndex}
-          side={side}
-          onSourceChange={this.handleSourceChange}
-        />,
-      );
-      rightHandPoints.push(
-        <Point
-          key={i}
-          type="Point"
-          index={pointIndex + 6 * direction}
-          side={side}
-          onSourceChange={this.handleSourceChange}
-        />,
-      );
-    }
-
-    return (
-      <View style={[styles.boardHalf, sourceStyle]}>
-        {leftHandPoints}
-        <Point type="Bar" index={barIndex} side={side} onSourceChange={this.handleSourceChange} />
-        {rightHandPoints}
-        <Home />
-      </View>
+  const leftHandPoints: JSX.Element[] = [];
+  const rightHandPoints: JSX.Element[] = [];
+  for (let i = 0; i < 6; i += 1) {
+    const pointIndex = startingPointIndex + i * direction;
+    leftHandPoints.push(
+      <Point
+        key={i}
+        type="Point"
+        index={pointIndex}
+        side={side}
+        onSourceChange={handleSourceChange}
+      />,
+    );
+    rightHandPoints.push(
+      <Point
+        key={i}
+        type="Point"
+        index={pointIndex + 6 * direction}
+        side={side}
+        onSourceChange={handleSourceChange}
+      />,
     );
   }
+
+  return (
+    <View style={[styles.boardHalf, sourceStyle]}>
+      {leftHandPoints}
+      <Point type="Bar" index={barIndex} side={side} onSourceChange={handleSourceChange} />
+      {rightHandPoints}
+      <Home />
+    </View>
+  );
 }
